83b20b164328563315ffcb1693814f4894376402
Pieter Germishuys <pieter@pieterg.com>
2020-02-26 20:17:16 +0100

    Parse Header of Chunk File

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,16 +1,48 @@
+use std::convert::TryInto;
+use uuid::Uuid;
 use std::{
     fs::File,
     io::prelude::*
 };
 
+#[derive(Debug)]
+pub struct Header {
+    file_type: u8,
+    version: u8,
+    chunk_size: i32,
+    chunk_start_number: i32,
+    chunk_end_number: i32,
+    is_scavenged: u8,
+    chunk_id: Uuid
+}
+
 pub struct Chunk {
-    location: String
+    pub header: Header,
+    pub location: String
 }
 
 impl Chunk {
     pub fn open(location: String) -> Chunk {
+        let mut file = File::open(location.clone()).unwrap();
+        let mut header_buffer = [0; 128];
+        file.read(&mut header_buffer).unwrap();
+        let order = [3, 2, 1, 0, 5, 4, 7, 6, 8, 9, 10, 11, 12, 13, 14, 15];
+        let mut uuid: [u8; 16] = [0; 16];
+        for n in 0..16 {
+            uuid[n] = header_buffer[18 + order[n]];
+        }
+        let header = Header {
+            file_type: header_buffer[0],
+            version: header_buffer[1],
+            chunk_size: i32::from_le_bytes(header_buffer[2..6].try_into().unwrap()),
+            chunk_start_number: i32::from_le_bytes(header_buffer[7..11].try_into().unwrap()),
+            chunk_end_number: i32::from_le_bytes(header_buffer[12..16].try_into().unwrap()),
+            is_scavenged: header_buffer[17],
+            chunk_id: Uuid::from_bytes(uuid.try_into().unwrap()),
+        };
         return Chunk {
-            location
+            header,
+            location: location.clone()
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod core;
 
 fn main() {
     let chunk = core::Chunk::open("D:\\databases\\first\\chunk-000000.000000".parse().unwrap());
+    println!("{:?}", &chunk.header);
 }
